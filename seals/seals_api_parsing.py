c6eb610483b39c3ed1c79aa04a00be69d49f76f1
Justin Johnson <jajohns@umn.edu>
2024-08-05 15:00:14 -0500

    NEXT UP: Finish parsing_input_api_md to df

--- seals/seals_api_parsing.py
+++ seals/seals_api_parsing.py
@@ -11,6 +11,14 @@ def assign_defaults_from_model_spec(input_object, model_spec_dict):
         if not hasattr(input_object, k):
             setattr(input_object, k, v)
             
+def parse_input_api_md(input_md_path):
+    lines = hb.read_path_as_list()
+    
+    for line in lines:
+        # NEXT UP: Finish parsing_input_api_md to df
+        print('line', line)
+    
+            
 # Make this follow model spec for all pre-processing and validation
 def assign_df_row_to_object_attributes(input_object, input_row):
     # srtip() 
@@ -175,121 +183,6 @@ def generate_scenarios_csv_and_put_in_input_dir(p):
     hb.create_directories(p.scenario_definitions_path)
     p.scenarios_df.to_csv(p.scenario_definitions_path, index=False)
 
-
-
-
-# Old approach. Now we want to replace this with model spec, but here is a bunch of stuff that has modelspec info in comments
-def set_attributes_to_default(p):
-    # Set all ProjectFlow attributes to SEALS default.
-    # This is used if the user has never run something before, and therefore doesn't
-    # have a scenario_definitions.csv in their input dir.
-    # This function will set the attributes, and can be paired with 
-    # generate_scenarios_csv_and_put_in_input_dir to write the file.
-
-    ###--- SET DEFAULTS ---###
-    
-    # String that uniquely identifies the scenario. Will be referenced by other scenarios for comparison.
-    p.scenario_label = 'ssp2_rcp45_luh2-globio_bau'
-
-    # Scenario type determines if it is historical (baseline) or future (anything else) as well
-    # as what the scenario should be compared against. I.e., Policy minus BAU.
-    p.scenario_type = 'bau'
-
-    # Sets the area of interest. If set as a country-ISO3 code, all data will be generated based
-    # that countries boundaries (as defined in the base data). Other options include setting it to
-    # 'global' or a specific shapefile, or iso3 code. Good small examples include RWA, BTN
-    p.aoi = 'RWA'
-
-    # Exogenous label references some set of exogenous drivers like population, TFP growth, LUH2 pattern, SSP database etc
-    p.exogenous_label = 'ssp2'
-
-    # One of the climate RCPs
-    p.climate_label = 'rcp45'
-
-    # Indicator of which model led to the coarse projection
-    p.model_label = 'luh2-message'
-
-    # AKA policy scenario, or a label of something that you have tweaked to assess it's efficacy
-    p.counterfactual_label = 'bau'
-
-    # If is not a baseline scenario, these are years into the future. Duplicate the base_year variable below if it is a base year
-    # From the csv, this is a space-separated list.
-    p.years = [2100]
-
-    # For calculating difference from base year, this references which baseline (must be consistent due to different
-    # models having different depictions of the base year)
-    p.baseline_reference_label = 'baseline_luh2-message'
-
-    # Which year in the observed data constitutes the base year. There may be multiple if, for instance, you
-    # want to use seals to downscale model outputs that update a base year to a more recent year base year
-    # which would now be based on model results but is for an existing year. Paper Idea: Do this for validation.
-    p.base_years = [2017]
-
-    # Even with multiple years, we will designate one as the key base year, which will be used e.g.
-    # for determining the resolution, extent, projection and other attributes of the fine resolution
-    # lulc data. It must refer to a year that has observed LULC data to load.
-    p.key_base_year = 2017
-
-
-    # If set to one of the other policies, like BAU, this will indicate which scenario to compare the performance
-    # of this scenario to. The tag 'no_policy' indicates that it should not be compared to anything.
-    p.comparison_counterfactual_labels = 'no_policy'
-
-    # Path to the coarse land-use change data. SEALS supports 2 types: a netcdf directory of geotiffs following
-    # the documented structure.
-    p.coarse_projections_input_path = "luh2/raw_data/rcp45_ssp2/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-MESSAGE-ssp245-2-1-f_gn_2015-2100.nc"
-    # p.coarse_projections_input_path = 'luh2/raw_data/rcp26_ssp1/multiple-states_input4MIPs_landState_ScenarioMIP_UofMD-IMAGE-ssp126-2-1-f_gn_2015-2100.nc'
-
-    # Label of the LULC data being reclassified into the simplified form.
-    p.lulc_src_label = 'esa'
-
-    # Label of the new LULC simplified classification
-    p.lulc_simplification_label = 'seals7'
-
-    # Path to a csv that will map the a many-to-one reclassification of
-    # the src LULC map to a simplified version
-    # includes at least src_id, dst_id, src_label, dst_label
-    p.lulc_correspondence_path = 'seals/default_inputs/esa_seals7_correspondence.csv'
-
-    # Label of the coarse LUC data that will be reclassified and downscaled
-    p.coarse_src_label = 'luh2-14'
-
-    # Label of the new coarse LUC data
-    p.coarse_simplification_label = 'seals7'
-
-    # Path to a csv that includes at least src_id, dst_id, src_label, dst_label
-    p.coarse_correspondence_path = 'seals/default_inputs/luh2-14_seals7_correspondence.csv'
-
-    # Often NetCDF files can have the time dimension in something other than just the year. This string allows
-    # for doing operations on the time dimension to match what is desired. e.g., multiply5 add2015
-    p.time_dim_adjustment = 'add2015'
-
-    # Because different NetCDF files have different arrangements (e.g. time is in the dimension
-    # versus LU_class is in the dimension), this option allows you to specify where in the input
-    # NC the information is. If 'all_variables', assumes the LU classes will be the different variables named
-    # otherwise it can be a subset of variables, otherwise, if it is a named variable, e.g. LC_area_share
-    # then assume that the lc_class variable is stored in the last-listed dimension (see p.dimensions)
-    p.lc_class_varname = 'all_variables'
-
-    # Lists which dimensions are stored in the netcdf in addition to lat and lon. Ideally
-    # this is just time but sometimes there are more.  # From the csv, this is a space-separated list.
-    p.dimensions = 'time'
-
-    # # To speed up processing, select which classes you know won't change. For default seals7, this is
-    # # the urban classes, the water classes, and the bare land class.
-    # p.nonchanging_class_indices = [0, 6, 7]
-
-    # Path to a csv which contains all of the pretrained regressor variables. Can also
-    # be 'from_calibration' indicating that this run will actually create the calibration``
-    # or it can be from a tile-designated file of location-specific regressor variables.
-    p.calibration_parameters_source = 'seals/default_inputs/default_global_coefficients.csv'
-    
-    # Some data, set to default inputs here, are required to make the model run becayse they determine which classes, which resolutions, ...
-    p.key_base_year_lulc_simplified_path = os.path.join('lulc', p.lulc_src_label, p.lulc_simplification_label, 'lulc_' + p.lulc_src_label + '_' + p.lulc_simplification_label + '_' + str(p.key_base_year) + '.tif')
-    p.key_base_year_lulc_src_path = os.path.join('lulc', p.lulc_src_label, 'lulc_' + p.lulc_src_label  + '_' + str(p.key_base_year) + '.tif')
-
-    # For convenience, set a synonym for the key_base_year_lulc_simplified_path
-    p.base_year_lulc_path = p.key_base_year_lulc_src_path
 
 
 # Not SURE if this is modelspec, but maybe it would allow custom functions after an input?
