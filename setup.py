6cbd1ad6ed656c3bd8483c6fa60b81410d13d3e3
James Douglass <jdouglass@stanford.edu>
2025-03-26 11:46:06 -0700

    Moving everything possible to pyproject.toml.
    This includes a directory rename from sealsmodel to seals.

--- setup.py
+++ setup.py
@@ -1,30 +1,8 @@
-import os
-from distutils.core import setup
-from distutils.extension import Extension
-
 import numpy
-from Cython.Build import cythonize
-from Cython.Distutils import build_ext
-from setuptools import find_packages
 from setuptools import setup
 
-packages=find_packages()
-include_package_data=True
-
-extensions = [
-        Extension(
-        "sealsmodel.seals_cython_functions",  # This corresponds to the Python import path
-        ["sealsmodel/seals_cython_functions.pyx"],  # Path to the .pyx file
-    )
-]
-
+# As of 2025-03-26 there doesn't appear to be a way to do the numpy build dir
+# inclusion in pyproject.toml.
 setup(
-    name = 'sealsmodel',
-    packages = packages,
-    version = '1.5.4',
-    # download_url = 'https://github.com/jandrewjohnson/hazelbean/releases/hazelbean_x64_py3.6.3/dist/hazelbean-0.3.0_x64_py3.6.3.tar.gz',
-    ext_modules=cythonize(extensions),
-    # ext_modules=[Extension("seals_cython_functions", ["seals/seals_cython_functions.c"]),],
     include_dirs=[numpy.get_include()],
-    cmdclass={'build_ext': build_ext},
 )
